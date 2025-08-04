c9e3a775b06dffcbb02cdd7117c1d0794e9227ca
Pedro Escapalete <pedroescapalete@gmail.com>
2025-08-04 09:23:58 -0300

    RPG Ayla  2

---
+++ ayla.js
@@ -0,0 +1,13 @@
+let vida = 60;
+let energia = 40;
+let xp = 800;
+let ouro = 200;
+let nivel = 3;
+
+if (vida >= 80) {
+    console.log("Ayla Está em ótima forma!");
+} else if (vida >= 40) {
+    console.log("Ayla está ferida, mas pode lutar");
+} else {
+    console.log("Ayla está em estado crítico, recue!");
+}
