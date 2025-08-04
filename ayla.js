b46e725d640515ebcac16f20e1a6480a95291026
Pedro Escapalete <pedroescapalete@gmail.com>
2025-08-04 09:35:50 -0300

    Ayla_Adventure

--- ayla.js
+++ ayla.js
@@ -11,3 +11,24 @@ if (vida >= 80) {
 } else {
     console.log("Ayla está em estado crítico, recue!");
 }
+
+if (vida >= 50 && energia >= 50){
+    console.log("Ayla entra no calabouço");
+} else {
+    console.log("Ayla não pode seguir para o calabouço!");
+}
+
+
+if (xp >= 1000) {
+    nivel++;
+    console.log(`Ayla subiu de nivel ${nivel}.`);
+} else {
+    console.log("Adquira mais xp para subir de nível");
+}
+
+
+if (ouro >= 300) {
+    console.log("Ayla comprou a armadura mágica"); 
+} else {
+    console.log("Ayla não tem ouro suficiente");
+}
